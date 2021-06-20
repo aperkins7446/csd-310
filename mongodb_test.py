45e1e02b29c0ecd9c6dc53df77afb609a8589971
Angela Perkins <aperkins@my365.bellevue.edu>
2021-06-20 15:17:19 -0700

    Module 5 Removed uneeded files

--- mongodb_test.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-url= "mongodb+srv://admin:admin@cluster0.9olja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-client = MongoClient(url)
-db = client.pytech
-print("-- Pytech Collection List--")
-print(db.list_collection_names())
