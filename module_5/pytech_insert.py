2f99e2d2e6bcb2e4e4680b34610f1fb239647379
Angela Perkins <aperkins@my365.bellevue.edu>
2021-06-20 16:41:56 -0700

    Module 5 Removed uneeded files

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,15 +1,28 @@
-MongoDB: insert_one() 
+#important pymongo and MongoClient
+import pymongo
+from pymongo import MongoClient
+
+#connect to the MongoDB
+url= "mongodb+srv://admin:admin@cluster0.9olja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+client = MongoClient(url)
+#tell it which database we are adding students to
+db = client.pytech
+students = db['students']
+
+#give it the student data
 fred = {
+ "student_id": "1007",
  "first_name": "Fred",
  "last_name": "Smith"
 }
- 
+#insert the student data 
 fred_1007 = students.insert_one(fred).inserted_id
  
 print(fred_1007)
 
-MongoDB: insert_one() 
+
 hazel = {
+ "student_id": "1008",
  "first_name": "Hazel",
  "last_name": "Hodge"
 }
@@ -17,8 +30,9 @@ hazel = {
 hazel_1008 = students.insert_one(hazel).inserted_id
  
 print(hazel_1008)
-MongoDB: insert_one() 
+
 angela = {
+"student_id": "1009",
  "first_name": "Angela",
  "last_name": "Perkins"
 }
--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,20 +1,23 @@
-MongoDB: find() 
-docs = db.collection_name.find({})
- 
-for doc in docs:
- print(doc)
- 
-MongoDB: find_one() 
-doc = db.collection_name.find_one({"student_id": 1007})
- 
-print(doc[1007])
 
-MongoDB: find_one() 
-doc = db.collection_name.find_one({"student_id": 1008})
- 
-print(doc[1008])
+#import pymongo and MongoClient
+from pymongo import MongoClient
+url= "mongodb+srv://admin:admin@cluster0.9olja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+client = MongoClient(url)
+#tell it which database we want to look at
+db = client.pytech
+students = db.students
 
-MongoDB: find_one() 
-doc = db.collection_name.find_one({"student_id": 1009})
- 
-print(doc[1009])
+#search for all students in the database
+student_list = students.find({})
+print("\n  -- All student documents returned with find() query --")
+for doc in student_list:
+#display information 
+   print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+#search for one student in the database
+hazel = students.find_one({"student_id": "1008"})
+#display information
+print("\n  -- Single student document returned with find_one() query --")
+print("  Student ID: " + hazel["student_id"] + "\n  First Name: " + hazel["first_name"] + "\n  Last Name: " + hazel["last_name"] + "\n")
+
+
