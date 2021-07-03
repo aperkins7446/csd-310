78176ad7ffaf70a89dd0a5e2d74b1120ee8a147e
Angela Perkins <aperkins@my365.bellevue.edu>
2021-07-03 16:06:53 -0700

    Module 8_revised

--- module_8/db_init.sql
+++ module_8/db_init.sql
@@ -1,14 +1,10 @@
---drop test use if exists
 DROP USER IF EXISTS 'pysports_user'@'localhost';
 
---crete pysports_user and grant them all privileges to the pysports database
-CREATE USER 'pysports_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+CREATE USER 'pysports_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Angel@84';
 
---drop tables if they are present
 DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS team;
 
---create team table
 CREATE TABLE team (
 	team_id		INT		NOT NULL		AUTO_INCREMENT,
 	team_name	VARCHAR(75)	NOT NULL,
@@ -16,7 +12,6 @@ CREATE TABLE team (
 	PRIMARY KEY(team_id)
 );
 
---create the player table and set the foreign key
 CREATE TABLE player (
 	player_id		INT		NOT NULL	AUTO_INCREMENT,
 	first_name		VARCHAR(75)	NOT NULL,
@@ -28,28 +23,26 @@ CREATE TABLE player (
 		REFERENCES team(team_id)
 );
 
---insert team records
 INSERT INTO team(team_name, mascot)
 	VALUES('Alliance', 'Noble Gnomes');
 
 INSERT INTO team(team_name, mascot)
 	VALUES('Horde', 'Terrifying Taurens');
 
---insert player records
 INSERT INTO player(first_name, last_name, team_id)
-	VALUES('Trixie', 'Tinker', (SELECT team_id FROM team WHERE team_name = 'Noble Gnomes'));
+	VALUES('Trixie', 'Tinker', (SELECT team_id FROM team WHERE team_name = 'Alliance'));
 
 INSERT INTO player(first_name, last_name, team_id)
-	VALUES('Tommy', 'Gnoneman', (SELECT team_id FROM team WHERE team_name = 'Noble Gnomes'));
+	VALUES('Tommy', 'Gnoneman', (SELECT team_id FROM team WHERE team_name = 'Alliance'));
 
 INSERT INTO player(first_name, last_name, team_id)
-	VALUES('Piru', 'Buzzincup', (SELECT team_id FROM team WHERE team_name = 'Noble Gnomes'));
+	VALUES('Piru', 'Buzzincup', (SELECT team_id FROM team WHERE team_name = 'Alliance'));
 
 INSERT INTO player(first_name, last_name, team_id)
-	VALUES('Vikka', 'Fulltail', (SELECT team_id FROM team WHERE team_name = 'Terrifying Taurens'));
+	VALUES('Vikka', 'Fulltail', (SELECT team_id FROM team WHERE team_name = 'Horde'));
 
 INSERT INTO player(first_name, last_name, team_id)
-	VALUES('Etu', 'Sharpsoar', (SELECT team_id FROM team WHERE team_name = 'Terrifying Taurens'));
+	VALUES('Etu', 'Sharpsoar', (SELECT team_id FROM team WHERE team_name = 'Horde'));
 
 INSERT INTO player(first_name, last_name, team_id)
-	VALUES('Kitwit', 'Clawsinger', (SELECT team_id FROM team WHERE team_name = 'Terrifying Taurens'));
+	VALUES('Kitwit', 'Clawsinger', (SELECT team_id FROM team WHERE team_name = 'Horde'));
